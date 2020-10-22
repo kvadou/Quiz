297d1ed54ac3d456d9eaffe661b5fab81b7e9900
DPK <dougkvamme@gmail.com>
2020-10-21 20:07:42 -0500

    updated index and javascript page

--- script.js
+++ script.js
@@ -1,44 +1,434 @@
-//let timeEl = document.querySelector("#time");
-//let mainEl = document.getElementById("main");
-let startQuizEl = document.querySelector("#start-quiz")
+//removing question duration
 
-//let totalSeconds = 0;
-//let secondsElapsed = 0;
-//let interval;
-
-// function setTime() {
-//    totalSeconds = 60;
-//}
-
-// function startTimer() {
-//    setTime();
+// pull in page objects
+let highscoreDiv = document.querySelector("#highscore");
+let gameTimerEl = document.querySelector("#gameTimer");
+let mainEl = document.querySelector("#details");
+let timerTab = document.querySelector("#timers");
 
 
-//  }
+let questionEl = document.querySelector("#question")
+let answersListEl = document.querySelector("#answer-list")
 
-startQuizEl.addEventListener("click", startTimer);
+// set global variables - how do we move these into localized
+let test = false;
+let score = 0;
+let quiz = {};
+let quizType = "";
 
+let gameDuration = 0;
+let gameSecElapsed = 0;
+let gameInterval;
 
+let questionDuration = 60;
+let questionSecElapsed = 0;
+let questionInterval;
 
-function startTimer(duration, display) {
-    let timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+// draw instruction
+init();
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+// var startButton = document.querySelector("#startQuiz");
 
-        display.textContent = minutes + ":" + seconds;
+// function to display instructions
+function init() {
+  clearDetails();
+  reset();
+  // creates Heading element for main page
+  let heading = document.createElement("p");
+  heading.setAttribute("id", "main-heading");
+  heading.textContent = " Coding Quiz Challenge ";
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+  // creates elements with the instructions for the game
+  let instructions = document.createElement("p");
+  instructions.setAttribute("id", "instructions");
+  instructions.textContent = " Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your score/time by ten seconds! "; 
+
+  // adding more question - this should move into loop or function
+  // creates button to start the game
+  let startJsQuiz = document.createElement("button");
+  startJsQuiz.setAttribute("id", "startJSQuiz");
+  startJsQuiz.setAttribute("class", "btn btn-secondary");
+  startJsQuiz.textContent= "Start Javascript Quiz";
+
+  mainEl.appendChild(heading);
+  mainEl.appendChild(instructions);
+  mainEl.appendChild(startJsQuiz);
+
+  startJsQuiz.addEventListener("click", function () {
+    quizType = "Java Script";
+    playQuiz(jsQuestions);
+  });
+
 }
 
-window.onload = function () {
-    let minutes = 60,
-        display = document.querySelector('#time');
-    startTimer(minutes, display);
-};
+// function to clear details element of all children
+function clearDetails() {
+  mainEl.innerHTML = "";
+}
+
+function reset() {
+  quizType = "";
+  score = 0;
+
+  gameDuration = 0;
+  gameSecElapsed = 0;
+  gameInterval;
+
+  questionDuration = 60;
+  questionSecElapsed = 0;
+  questionInterval;
+}
+
+//start game
+function playQuiz(questionSet) {
+  if (test) { console.log("--- playQuiz ---"); }
+  // select quiz randomize questions
+  
+  quiz = setUpQuestions(questionSet);
+
+  // displays timers
+  timerTab.setAttribute("style", "visibility: visible;");
+
+  // Start timers here
+  gameDuration = quiz.length * 10;
+  if (test) { console.log("duration g,q:",gameDuration,questionDuration); }
+
+  startGameTimer();
+  renderTime();
+
+  //go to first question
+  presentQuestion();
+}
+
+// function to get random question out of array
+function setUpQuestions(arr) {
+  if (test) {console.log("--- setUpQuestions ---");}
+
+  let ranQuest = [];
+
+  for (let i=0; i<arr.length; i++) {
+    ranQuest.push(arr[i]);
+  }
+  return ranQuest;
+}
+
+// function to redraw screen with  question 
+function presentQuestion() {
+  if (test) {console.log("--- presentQuestion ---");}
+  // if (test) {console.log("cur.choices[i] " + cur.choices);}
+
+  //reset time allows to answer question
+  questionSecElapsed = 0;
+
+  // checks for no more questions and exits
+  if ( quiz.length === 0 ) {
+    endOfGame();
+    return;
+  }
+
+  // call question timer here
+  // reduceQUiz global
+
+  //sets current object (cur - question) by pulling out of reducedQuiz array leaving the remaining quetions in the array
+  curQuestion = quiz.pop();
+
+  //clears html to draw questions
+  clearDetails();
+   
+  // add question to screen
+  //build out display for new item
+  let question = document.createElement("h1");
+  // adds data value
+  question.setAttribute("question", curQuestion.title);
+  question.textContent = curQuestion.title;
+  mainEl.appendChild(question)
+
+  // create list as container to listen for answers
+  let choiceBox = document.createElement("ul");
+  choiceBox.setAttribute("id","choiceBox");
+  mainEl.appendChild(choiceBox);
+
+  //adds answers to screen
+  for( let i=0; i<curQuestion.choices.length; i++ ) {
+    // creates variable for each choice item
+    let listChoice = document.createElement("li");
+    // adds data value
+    listChoice.setAttribute("choice-value", curQuestion.choices[i]);
+    listChoice.setAttribute("id","questionNum-"+i);
+    listChoice.textContent = curQuestion.choices[i];
+    //add choice to page
+    choiceBox.appendChild(listChoice)
+  }
+
+  if (test) { console.log("cur", curQuestion);}
+
+  // get answer from user
+  // using the anymous function delays the invocation of the scoreAnswer
+  choiceBox.addEventListener("click", function (){
+    scoreAnswer(curQuestion);
+  });
+  // calls for the next questions
+}
+
+function scoreAnswer(cur) {
+  if (test) { console.log("--- scoreAnswer ---");}
+ // ensure that the event on the li
+  let e = event.target;
+  if ( e.matches("li")) {
+    let selectedItem = e.textContent;
+    // if (test) { console.log("check quiz " + quiz.length); }
+    if (test) { console.log("selectedItem quiz " + selectedItem); }
+    // if (test) { console.log("selectedItem cur " , cur.answer); }
+    if ( selectedItem === cur.answer ) {
+      // if (test) { console.log("correct answer");}
+      score += questionDuration - questionSecElapsed;
+      //TODO music 
+    } else {
+      if (test) { console.log("wrong answer");}
+      //penelty for being wrong
+      gameDuration -= 10;
+    }
+  if (test) { console.log("sselected ",selectedItem);}
+    showAnswers(cur);
+    // presentQuestion();
+  }
+}
+
+// TODO incomplete does not disply the correct color!!!! arghh
+function showAnswers(cur) {
+  if (test) { console.log("--- showAnswer ---"); }
+  // if (test) { console.log("sa length",cur.choices.length);}
+  if (test) { console.log("sa qanda",cur);}
+  if (test) { console.log("sselected ",selectedItem);}
+
+
+  for (let i=0; i<cur.choices.length; i++) {
+    if (test) { console.log("sa in for ",i);}
+
+    let questid = "#questionNum-" + i;
+    // if (test) { console.log("sa qn", questid );}
+    let questrow = document.querySelector(questid);
+
+    // if (test) { console.log("questrow",questrow);}
+
+    if (test) { console.log("saf selected" + selectedItem + "<");}
+    if (test) { console.log("saf color test >" +  cur.choices[i] +"<");}
+
+    if ( cur.choices[i] !== cur.answer ) {
+      if (test) { console.log("color test flase");}
+      questrow.setAttribute("style","background-color: red");
+    } else {
+      if (test) { console.log("color test true");}
+      questrow.setAttribute("style","background-color: green");
+    }
+  }
+  // pause so user can see results
+  setTimeout(presentQuestion,500);
+}
+
+// function to set time for game timer
+function setGameTime() {
+  if (test) { console.log("--- setGameTime ---"); }
+  if (test) { console.log("gameDuration " + gameDuration); }
+  clearInterval(gameInterval);
+  gameSeconds = gameDuration;
+}
+
+
+function renderTime() {
+  // if (test) { console.log(" --- renderTime --- "); }
+  // if (test) { console.log("gameSecElapsed " + gameSecElapsed); }
+  // if (test) { console.log("gameDuration " + gameDuration); }
+  // if (test) { console.log("questionDuration " + questionDuration); }
+
+  gameTimerEl.textContent = gameDuration - gameSecElapsed;
+
+  if ( (questionDuration - questionSecElapsed) < 1 ) {
+    // game penelty for letting timer run out
+    gameDuration -= 10;
+    if (test) { console.log("too slow"); }
+    presentQuestion();
+  } 
+
+  if ( (gameDuration - gameSecElapsed) < 1 ) {
+   endOfGame();
+  }
+}
+
+function startGameTimer () {
+  if (test) { console.log("--- startGameTimer ---"); }
+  setGameTime();
+
+  gameInterval = setInterval(function() {
+    gameSecElapsed++; 
+    questionSecElapsed++; 
+    renderTime();
+  }, 1000);
+}
+
+function stopTime() {
+  if (test) { console.log("--- stopTime --- ");}
+  gameSeconds = 0;
+  questionSeconds = 0;
+  clearInterval(gameInterval);
+}
+
+// function of end of game
+function endOfGame() {
+  if (test) { console.log("--- endOfGame ---"); }
+  stopTime();
+  clearDetails();
+
+  timerTab.setAttribute("style", "visibility: hidden;");
+
+  let heading = document.createElement("p");
+  heading.setAttribute("id", "main-heading");
+  heading.textContent = "GAME OVER - I hope you have enjoyed this";
+
+  // creates elements with the instructions for the game
+  let instructions = document.createElement("p");
+  instructions.setAttribute("id", "instructions");
+  instructions.textContent = " Your score is " + score; 
+
+  // creates button to start the game
+  let playAgain = document.createElement("button");
+  playAgain.setAttribute("id", "playAgain");
+  playAgain.setAttribute("class", "btn btn-secondary");
+  playAgain.textContent = "Play again";
+
+  // creates input for user to add initials
+  let par = document.createElement("p");
+
+  let initialsLabel = document.createElement("label");
+  initialsLabel.setAttribute("for","userInitials");
+  initialsLabel.textContent = "Enter Initials: ";
+
+  let initialsInput = document.createElement("input");
+  initialsInput.setAttribute("id","userInitials");
+  initialsInput.setAttribute("name","userInitials");
+  initialsInput.setAttribute("minlength","3");
+  initialsInput.setAttribute("maxlength","3");
+  initialsInput.setAttribute("size","3");
+
+
+  mainEl.appendChild(heading);
+  mainEl.appendChild(instructions);
+  mainEl.appendChild(initialsLabel);
+  mainEl.appendChild(initialsInput);
+  mainEl.appendChild(par);
+  mainEl.appendChild(playAgain);
+
+  playAgain.addEventListener("click", init);
+
+  initialsInput.addEventListener("input", function() {
+    initialsInput.value = initialsInput.value.toUpperCase();
+    if ( initialsInput.value.length === 3 ) { 
+
+      //create object for this score
+      let thisScore = [ { type: quizType, name: initialsInput.value, score: score } ]; 
+
+      //get highscores from memory
+      let storedScores = JSON.parse(localStorage.getItem("highScores")); 
+      if (test) { console.log("storedScore",storedScores); }
+
+      if (storedScores !== null) { 
+        storedScores.push(thisScore[0]); 
+      } else {
+        storedScores = thisScore;
+      }
+
+      localStorage.setItem("highScores", JSON.stringify(storedScores));
+      highScores();
+    }
+  });
+}
+
+function highScores() {
+  stopTime();
+  clearDetails();
+
+  timerTab.setAttribute("style", "visibility: hidden;");
+
+  //get scores from storage
+  let storedScores = JSON.parse(localStorage.getItem("highScores")); 
+
+  // draw heading
+  let heading = document.createElement("h2");
+  heading.setAttribute("id", "main-heading");
+  heading.textContent = "Top 5 High Score Hall of Fame";
+
+  mainEl.appendChild(heading);
+
+  // Render a new li for each score
+  // TODO check for this error 
+  if ( storedScores !== null ) {
+    // sort scores
+    storedScores.sort((a,b) => (a.score < b.score) ? 1: -1);
+
+    // sets the number of scores to display to 5 or the number of games played. Which ever is less
+    let numScores2Display = 5;
+    if ( storedScores.length < 5 ) { 
+      numScores2Display = storedScores.length; 
+    }
+
+    for (let i = 0; i < numScores2Display; i++) {
+      let s = storedScores[i];
+
+      let p = document.createElement("p");
+      p.textContent = s.name + " " + s.score + " ( " + s.type + " )";
+      mainEl.appendChild(p);
+    }
+  } else {
+    let p = document.createElement("p");
+    p.textContent =  "Your Initials Here!"
+    mainEl.appendChild(p);
+  }
+
+
+  // creates button to start the game
+  let playAgain = document.createElement("button");
+  playAgain.setAttribute("id", "playAgain");
+  playAgain.setAttribute("class", "btn btn-secondary");
+  playAgain.textContent = "Play!";
+
+  mainEl.appendChild(playAgain);
+
+  playAgain.addEventListener("click", init);
+}
+
+highscoreDiv.addEventListener("click", highScores);
+
+let jsQuestions = [
+    {
+      title: "JavaScript was originally developed to be used with what browswer?",
+      choices: ["Google Chrome", "Netscape Navigator", "Microsoft Internet Explorer", "Mozilla Firefox"],
+      answer: "Netscape Navigator"
+    },
+    {
+      title: "JavaScript is a registered trademark of what US Corporation?",
+      choices: ["Mozilla", "Google", "Oracle", "Microsoft"],
+      answer: "Oracle"
+    },
+    {
+      title: "Which of the following JavaScript frameworks/libraries was released earliest?",
+      choices: ["Angular.js", "Backbone.js", "Vue.js", "React.js"],
+      answer: "Backbone.js"
+    },
+    
+    {
+      title: "JavaScript was originally called LiveScript  when first shipped as part of a Navigator release in September 1995.",
+      choices: ["True", "False"],
+      answer: "True"
+    },
+    {
+      title: "Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web.",
+      choices: ["True", "False"],
+      answer: "True"
+    },
+    {
+      title: "Node.js was originally written by:",
+      choices: ["Brendan Eich", "Jack Dorsey", "Ryan Dahl", "Mark Zuckerberg"],
+      answer: "Ryan Dahl"
+    },
+  ];
+  
